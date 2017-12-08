67c8cc1d6d4df52eb4d6c95024cf81db35e7f725
farrellkj2 <farrellkj2@gmail.com>
2017-12-08 14:30:06 -0500

    push updates to module testing files

--- CrossScale Emergence/Module_test_files/R_Script_noNotes.R
+++ CrossScale Emergence/Module_test_files/R_Script_noNotes.R
@@ -1,5 +1,5 @@
 library(glmtools)
-sim_folder <- 'C:/Users/KFarrell/Desktop/R/ProjectEDDIE/CrossScale Emergence/Mendota' 
+sim_folder <- 'C:/Users/KFarrell/Desktop/R/ProjectEDDIE/CrossScale Emergence/KF_module_test_files/Mendota' 
 setwd(sim_folder)
 
 ########## BASELINE #############
@@ -58,7 +58,7 @@ plot_var(file=climate_landuse, "PHY_TCHLA")
 ########## LINE PLOTS #############
 attach(chla_output)
 
-plot(DateTime, Baseline_Chla, type="l", col="black", ylim=c(0, 5), ylab="Chlorophyll-a (ug/L)", xlab="Date")  
+plot(DateTime, Baseline_Chla, type="l", col="black", ylim=c(0, 500), ylab="Chlorophyll-a (ug/L)", xlab="Date")  
 lines(DateTime, Climate_Chla, col="red") 
 lines(DateTime, LandUse_Chla, col="blue") 
 lines(DateTime, Climate_LandUse_Chla, col="green") 
@@ -70,7 +70,9 @@ library(ggplot2)
 mytheme <- theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), 
                  axis.line.x = element_line(colour = "black"), axis.line.y = element_line(colour = "black"), 
                  axis.text.x=element_text(size=15, colour='black'), axis.text.y=element_text(size=15, colour='black'), 
-                 axis.title.x=element_text(size=18), axis.title.y=element_text(size=18))
+                 axis.title.x=element_text(size=18), axis.title.y=element_text(size=18), 
+                 plot.title = element_text(size=22, hjust=.5, face='bold'),
+                 legend.text=element_text(size=14))
 
 chla <- melt(chla_output, id.vars=c("DateTime"))
 chla$variable <- factor(chla$variable, levels=c('Baseline_Chla','Climate_Chla','LandUse_Chla','Climate_LandUse_Chla'), labels=c('Baseline','Climate','Land Use','Climate & Land Use'))
@@ -81,3 +83,31 @@ ggplot(chla, aes(y = value, x = DateTime, colour=variable))+ mytheme+
   scale_x_datetime(date_breaks = '6 months',  date_labels = "%b-%Y") +
   labs(y= expression(Chlorophyll-a~~(ug~~L^-1)), x= "Date")+
   theme(legend.position=c(0,1), legend.justification = c(-.1,1), legend.key=element_blank(), legend.title=element_blank())
+
+#### Example plots for PPT ####
+# Synergy, additive, antagonistic
+effects <- read.csv('./effects_example.csv')
+
+eff <- melt(effects, id.vars=c("Type","DateTime"))
+eff$variable <- factor(eff$variable, levels=c('Baseline_Chla','Climate_Chla','LandUse_Chla','Climate_LandUse_Chla'), 
+                       labels=c('Baseline','Climate','Land Use','Climate & Land Use'))
+eff$DateTime <- as.Date(eff$DateTime, format='%Y-%m-%d')
+eff$DateTime <- as.POSIXct(eff$DateTime)
+
+ggplot(subset(eff, Type=="additive"), aes(y = value, x = DateTime, colour=variable))+ mytheme+ 
+  geom_line(lwd=1) +  ggtitle("Additive effects of \n climate and land use change") + 
+  scale_x_datetime(date_breaks = '6 months',  date_labels = "%b-%Y") +
+  labs(y= expression(Chlorophyll-a~~(ug~~L^-1)), x= "Date")+
+  theme(legend.position=c(1,1), legend.justification = c(1,1), legend.key=element_blank(), legend.title=element_blank())
+
+ggplot(subset(eff, Type=="antagon"), aes(y = value, x = DateTime, colour=variable))+ mytheme+ 
+  geom_line(lwd=1) + ggtitle("Antagonistic effects of \n climate and land use change") + 
+  scale_x_datetime(date_breaks = '6 months',  date_labels = "%b-%Y") +
+  labs(y= expression(Chlorophyll-a~~(ug~~L^-1)), x= "Date")+
+  theme(legend.position=c(1,1), legend.justification = c(1,1), legend.key=element_blank(), legend.title=element_blank())
+
+ggplot(subset(eff, Type=="synergy"), aes(y = value, x = DateTime, colour=variable))+ mytheme+ 
+  geom_line(lwd=1) + ggtitle("Synergistic effects of \n climate and land use change") + 
+  scale_x_datetime(date_breaks = '6 months',  date_labels = "%b-%Y") +
+  labs(y= expression(Chlorophyll-a~~(ug~~L^-1)), x= "Date")+
+  theme(legend.position=c(1,1), legend.justification = c(1,1), legend.key=element_blank(), legend.title=element_blank())
