5e55dab750ede754751bff37e3227f9162e5d728
KJ Farrell <farrellkj2@gmail.com>
2018-03-07 13:33:55 -0500

    rough script for teleconnection sites map

--- EDDIE_users_Map.R
+++ EDDIE_users_Map.R
@@ -1,7 +1,25 @@
 # install.packages('pacman')
-pacman::p_load(maps)
+pacman::p_load(maps, ggmap, mapproj)
 
-map('world', xlim=c(-180,90), ylim=c(10,90), col = 'grey90', fill = T,border = 'grey50',lwd=0.3)
-map('state', col = 'grey90', fill = T,border = 'grey50',lwd=0.3, add=T)
-map('world', regions=c('Ireland'), add=T, col = 'dodgerblue', fill=T)
-map('state', regions=c('virginia','new york','connecticut','maine','illinois'), add=T, col = 'dodgerblue', fill=T)
+# Ugly world slice to include Ireland
+par(mar = c(0,0,0,0),mgp=c(0,0,0))
+maps::map('world', xlim=c(-180,90), ylim=c(10,90), col = 'grey90', fill = T,
+          interior=F, border = 'grey50',lwd=0.3)
+maps::map('state', col = 'grey90', fill = T,border = 'grey50',lwd=0.3, add=T)
+maps::map('world', regions=c('Ireland'), add=T, col = 'dodgerblue', fill=T)
+maps::map('state', regions=c('virginia','ohio', 'new york','illinois', 'washington', 'maine'), add=T, col = 'dodgerblue', fill=T)
+maps::map('state', regions=c('virginia','connecticut', 'washington dc'), 
+          add=T, col = 'tomato', fill=T) # Module 2
+
+# US only
+par(mar = c(0,0,0,0),mgp=c(0,0,0))
+maps::map('state', col = 'grey90', fill = T,border = 'grey50',lwd=0.3)
+maps::map('state', regions=c('virginia','ohio', 'new york','illinois', 'washington', 'maine'), 
+          add=T, col = 'dodgerblue', fill=T) # Module 1
+maps::map('state', regions=c('virginia','connecticut', 'washington dc'), 
+          add=T, col = 'tomato', fill=T) # Module 2
+
+
+
+map <- get_map(location = 'Europe', zoom = 4)
+ggmap(map)
---
+++ Teleconnections/TeleC_Lakes_Map.R
@@ -0,0 +1,54 @@
+# Map of lakes included in Teleconnections Module
+
+# install.packages('pacman')
+pacman::p_load(maps)
+
+data <- read.csv('./Teleconnections/LakeLocations.csv')
+
+# NEON + other lake sites on US map ####
+par(mar = c(0,0,0,0),mgp=c(0,0,0))
+maps::map("world", c("USA", "alaska"), xlim=c(-180,-65), ylim=c(19,72), col='grey90',
+          fill=T, border='grey50', wrap=T,lwd=0.3) # background USA map
+maps::map('state', region=c('florida', 'new hampshire','wisconsin','north dakota'), 
+    add = T, border='grey50',col='lightblue', fill=T, lwd=0.5) # States from conterminous US
+maps::map("world", c("USA:Alaska"), add = T, border='grey50',col='lightblue', fill=T, lwd=0.5) # Alaska
+maps::map("world", c("USA:Hawaii"), add = T, border='white',col='white', fill=T, lwd=0.5) # Hide Hawaii
+points(data$Long,data$Lat,col='black', pch=16, cex=1,lwd=1)
+
+# Just Conterminous sites.
+par(mar = c(0,0,0,0),mgp=c(0,0,0))
+maps::map('usa', col='grey90',fill=T, border='grey50', wrap=T,lwd=0.3) # background USA map
+maps::map('state', region=c('florida', 'new hampshire','wisconsin','north dakota'), 
+          add = T, border='grey50',col='lightblue', fill=T, lwd=0.5) # States from conterminous US
+points(data$Long,data$Lat,col='black', pch=16, cex=1,lwd=1)
+
+# State-specific maps ####
+# Wisconsin
+par(mar = c(0,0,0,0),mgp=c(0,0,0))
+maps::map('state', region=c('wisconsin'), lwd=2)
+points(data$Long,data$Lat, cex=1.5,lwd=1, pch=16, col='black')
+text(c(-88.52, -89.1, -89), c(42.9, 45.9, 46.3), c("Mendota", "Little Rock", "Crampton"))
+
+# Florida
+par(mar = c(0,0,0,0),mgp=c(0,0,0))
+maps::map('state', region=c('florida'), lwd=2)
+points(data$Long,data$Lat, cex=1.5,lwd=1, pch=16, col='black')
+# Add text for site names
+
+# North Dakota
+par(mar = c(0,0,0,0),mgp=c(0,0,0))
+maps::map('state', region=c('north dakota'), lwd=2)
+points(data$Long,data$Lat, cex=1.5,lwd=1, pch=16, col='black')
+# Add text for site names
+
+# New Hampshire
+par(mar = c(0,0,0,0),mgp=c(0,0,0))
+maps::map('state', region=c('new hampshire'), lwd=2)
+points(data$Long,data$Lat, cex=1.5,lwd=1, pch=16, col='black')
+# Add text for site names
+
+# Alaska
+par(mar = c(0,0,0,0),mgp=c(0,0,0))
+maps::map("world", c("USA:Alaska"), lwd=2)
+points(data$Long,data$Lat, cex=1.5,lwd=1, pch=16, col='black')
+# Add text for site names
