b4b91c5b0b1e5db5f878981eb72c22add247b6fd
farrellkj2 <farrellkj2@gmail.com>
2018-10-09 13:01:52 -0400

    script and lat/long files for EDDIE users (useful for PPT presentation maps)

--- EDDIE_users_Map.R
+++ EDDIE_users_Map.R
@@ -1,13 +1,13 @@
 # install.packages('pacman')
-pacman::p_load(maps, ggmap, mapproj)
+pacman::p_load(maps, ggmap, mapproj, tidyverse)
 
 tester_names <- c('Arrupe', 'Colby', 'Dundalk', 'Fairfield', 
                   'Georgetown', 'Longwood', 'Michigan State', 'SUNY-New Paltz',
                   'Miami OH', 'VT', 'WSU')
 tester_states <- c('Illinois','Maine','Connecticut','Virginia','Michigan','New York',
                    'Ohio','Washington', 'Washington DC')
-lat <- c()
-long <- c()
+
+sites <- read_csv('./UsersLatLong.csv')
 
 # Ugly world slice to include Ireland ####
 par(mar = c(0,0,0,0),mgp=c(0,0,0))
@@ -28,12 +28,16 @@ maps::map('state', regions=c('virginia','connecticut', 'washington dc'),
           add=T, col = 'tomato', fill=T) # Module 2
 
 # All testers to date for ESA ####
+US_sites <- sites %>% filter(School != "Dundalk")
 par(mar = c(0,0,0,0),mgp=c(0,0,0))
 maps::map('state', col = 'grey90', fill = T,border = 'grey50',lwd=0.3)
 maps::map('state', regions= tester_states, 
           add=T, col = 'dodgerblue', fill=T)
+points(US_sites$Longitude, US_sites$Latitude,col='black', pch=18, cex=2,lwd=1)
 
+Ireland <- sites %>% filter(School == 'Dundalk')
 par(mar = c(0,0,0,0),mgp=c(0,0,0))
 maps::map('world', regions=c('Ireland', 'UK'), col = 'grey90', fill = T,
           border = 'grey50',lwd=0.3)
 maps::map('world', regions = c('Ireland'), col= 'dodgerblue', fill= T, add=T)
+points(Ireland$Longitude, Ireland$Latitude,col='black', pch=18, cex=2,lwd=1)
