7f231c076eb7ee39c3636700aded9b17ba3d7b89
farrellkj2 <farrellkj2@gmail.com>
2018-11-01 16:53:22 -0400

    add new users to MacroEDDIE map for G20 poster

--- EDDIE_users_Map.R
+++ EDDIE_users_Map.R
@@ -1,13 +1,10 @@
 # install.packages('pacman')
 pacman::p_load(maps, ggmap, mapproj, tidyverse)
 
-tester_names <- c('Arrupe', 'Colby', 'Dundalk', 'Fairfield', 
-                  'Georgetown', 'Longwood', 'Michigan State', 'SUNY-New Paltz',
-                  'Miami OH', 'VT', 'WSU')
-tester_states <- c('Illinois','Maine','Connecticut','Virginia','Michigan','New York',
-                   'Ohio','Washington', 'Washington DC')
-
 sites <- read_csv('./UsersLatLong.csv')
+teserstates <- 
+
+
 
 # Ugly world slice to include Ireland ####
 par(mar = c(0,0,0,0),mgp=c(0,0,0))
@@ -15,29 +12,23 @@ maps::map('world', xlim=c(-180,90), ylim=c(10,90), col = 'grey90', fill = T,
           interior=F, border = 'grey50',lwd=0.3)
 maps::map('state', col = 'grey90', fill = T,border = 'grey50',lwd=0.3, add=T)
 maps::map('world', regions=c('Ireland'), add=T, col = 'dodgerblue', fill=T)
-maps::map('state', regions=c('virginia','ohio', 'new york','illinois', 'washington', 'maine'), add=T, col = 'dodgerblue', fill=T)
-maps::map('state', regions=c('virginia','connecticut', 'washington dc'), 
-          add=T, col = 'tomato', fill=T) # Module 2
+maps::map('state', regions= sites$State, add=T, col = 'dodgerblue', fill=T)
 
 # US only
 par(mar = c(0,0,0,0),mgp=c(0,0,0))
 maps::map('state', col = 'grey90', fill = T,border = 'grey50',lwd=0.3)
-maps::map('state', regions=c('virginia','ohio', 'new york','illinois', 'washington', 'maine'), 
-          add=T, col = 'dodgerblue', fill=T) # Module 1
-maps::map('state', regions=c('virginia','connecticut', 'washington dc'), 
-          add=T, col = 'tomato', fill=T) # Module 2
+maps::map('state', regions= sites$State,add=T, col = 'dodgerblue', fill=T) # Module 1
 
 # All testers to date for ESA ####
 US_sites <- sites %>% filter(School != "Dundalk")
 par(mar = c(0,0,0,0),mgp=c(0,0,0))
-maps::map('state', col = 'grey90', fill = T,border = 'grey50',lwd=0.3)
-maps::map('state', regions= tester_states, 
-          add=T, col = 'dodgerblue', fill=T)
+maps::map('state', col = 'grey90', fill = T,border = 'black',lwd=1)
+maps::map('state', regions= sites$State,add=T, col = 'dodgerblue', fill=T)
 points(US_sites$Longitude, US_sites$Latitude,col='black', pch=18, cex=2,lwd=1)
 
 Ireland <- sites %>% filter(School == 'Dundalk')
 par(mar = c(0,0,0,0),mgp=c(0,0,0))
 maps::map('world', regions=c('Ireland', 'UK'), col = 'grey90', fill = T,
-          border = 'grey50',lwd=0.3)
+          border = 'grey50',lwd=1)
 maps::map('world', regions = c('Ireland'), col= 'dodgerblue', fill= T, add=T)
 points(Ireland$Longitude, Ireland$Latitude,col='black', pch=18, cex=2,lwd=1)
