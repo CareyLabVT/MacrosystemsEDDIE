27cb1cae02cad30ea93f71dd3c0c0f4661d20d9b
farrellkj2 <farrellkj2@gmail.com>
2018-11-14 10:45:28 -0500

    update to Tier 1 sites map

--- EDDIE_users_Map.R
+++ EDDIE_users_Map.R
@@ -1,10 +1,7 @@
 # install.packages('pacman')
 pacman::p_load(maps, ggmap, mapproj, tidyverse)
 
-sites <- read_csv('./UsersLatLong.csv')
-teserstates <- 
-
-
+sites <- read_csv('./UsersLatLong.csv') %>% filter(School != 'Wellesley')
 
 # Ugly world slice to include Ireland ####
 par(mar = c(0,0,0,0),mgp=c(0,0,0))
@@ -19,7 +16,7 @@ par(mar = c(0,0,0,0),mgp=c(0,0,0))
 maps::map('state', col = 'grey90', fill = T,border = 'grey50',lwd=0.3)
 maps::map('state', regions= sites$State,add=T, col = 'dodgerblue', fill=T) # Module 1
 
-# All testers to date for ESA ####
+# All testers to date ####
 US_sites <- sites %>% filter(School != "Dundalk")
 par(mar = c(0,0,0,0),mgp=c(0,0,0))
 maps::map('state', col = 'grey90', fill = T,border = 'black',lwd=1)
