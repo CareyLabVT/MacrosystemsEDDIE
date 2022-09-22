5efcbaf064d1595c110ad61430b9b45b2016ef90
wwoel <wwoelmer@vt.edu>
2022-09-22 15:21:53 -0400

    update materials and zenodo DOI

--- EDI_Publishing/Macrosystems_EDDIE_Module_8_DecisionSupport/EDI_publishing_Mod8.R
+++ EDI_Publishing/Macrosystems_EDDIE_Module_8_DecisionSupport/EDI_publishing_Mod8.R
@@ -99,7 +99,7 @@ make_eml(path = sim_folder,
          maintenance.description = "Completed", 
          user.id = "ccarey",
          user.domain = 'EDI',
-         package.id = "edi.808.2") # Put your package.id here, followed by .1 (for 1st version)
+         package.id = "edi.963.1") # Put your package.id here, followed by .1 (for 1st version)
 
 ## Step 8: Check your data product! ####
   # Return to the EDI staging environment (https://portal-s.edirepository.org/nis/home.jsp),
@@ -141,14 +141,14 @@ make_eml(path = sim_folder,
          other.entity.name = c("instructor_materials"),
          other.entity.description = c("This zip folder contains materials for instructors to teach the Macrosystems EDDIE 
                                  module in their classroom. See README file for file types and descriptions"),
-         temporal.coverage = c("2022-01-23", "2022-03-17"),
+         temporal.coverage = c("2022-01-23", "2022-09-22"),
          # keep geographic site as Derring for all MS EDDIE modules
          geographic.description = c("The Department of Biological Sciences at Virginia Tech is located in Blacksburg, Virginia, USA"),
          geographic.coordinates = c('37.229596', '-80.424863', '37.22854', '-80.426228'), #N, E, S, W
          maintenance.description = "Completed", 
          user.id = "ccarey",
          user.domain = 'EDI',
-         package.id = "edi.1106.2") # Put your package.id here, followed by .1 (for 1st version)
+         package.id = "edi.1106.3") # Put your package.id here, followed by .1 (for 1st version)
 
   # Once your xml file with your PUBLISHED package.id is Done, return to the 
   # EDI Production environment (https://portal.edirepository.org/nis/home.jsp)
